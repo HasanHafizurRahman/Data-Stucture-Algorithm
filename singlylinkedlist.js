3d4356d06cbf1285b160192a64e4437adb103a80
Hasan Shanto <hasanshanto922@gmail.com>
2022-11-13 16:48:42 +0600

    shift-unshift

--- singlylinkedlist.js
+++ singlylinkedlist.js
@@ -1,3 +1,11 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+
 class LinkedList {
     constructor() {
         this.head = null;
@@ -9,10 +17,8 @@ class LinkedList {
     }
 
     push(value) {
-        let newNode = {
-            value: value,
-            next: null
-        }
+        let newNode = new Node(value);
+
         if(this.isEmpty()) {
             this.head = newNode;
             this.tail = newNode;
@@ -53,16 +59,55 @@ class LinkedList {
         return lastNode;
     }
 
+    shift(){
+        if (!this.head) {
+            return null
+        }
+
+        const nodeToRemove = this.head;
+        this.head = this.head.next;
+        this.length--;
+
+        if (!this.length) {
+            this.tail = null;
+          }
+
+        return nodeToRemove;
+    }
+
+    unshift(value){
+        let newNode = new Node(value);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else{
+            newNode.next = this.head;
+            this.head = newNode
+        }
+        this.length++;
+    }
 }
 
 let list = new LinkedList();
 
+
+
+
 list.push(10)
 list.push(15)
 list.push(20)
+list.push(95)
 list.push(25)
+list.push(125)
+// list.push(35)
 
+
+list.shift();
+list.shift();
 list.pop();
 list.pop();
+list.unshift(5)
+list.unshift(45)
 // console.log(list.isEmpty());
 console.log(list);
